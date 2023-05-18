c847e133a39cd11d6a4c8c4e140d83582ee38dcf
vadimcrmnl1 <you@example.com>
2023-05-19 00:33:34 +0300

    finished project without adaptive UI, fix bugs

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './app/App'
 import { store } from './app/store'
@@ -15,11 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
   // </React.StrictMode>
 )
 
